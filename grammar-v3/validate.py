acb5826f08886c2074431df699fa7a1064ad5b09
Sébastien Lerique <sl@mehho.net>
2014-09-17 00:47:19 +0200

    Finish type testing on questions

--- grammar-v3/validate.py
+++ grammar-v3/validate.py
@@ -133,6 +133,56 @@ class Choice:
         self.tc.checkInstance(self, 'string', str)
 
 
+class Hint:
+
+    def __init__(self, tc, i, loaded, parent):
+        # The test case
+        self.tc = tc
+
+        # Error message suffix
+        self.name_err = 'hint #{} in '.format(i) + parent.name_err
+
+        # Fill members
+        self.string = loaded
+
+    def test_types(self):
+        self.tc.checkInstance(self, 'string', str)
+
+
+class Slider:
+
+    def __init__(self, tc, i, loaded, parent):
+        # The test case
+        self.tc = tc
+
+        # Error message suffix
+        self.name_err = 'slider #{} in '.format(i) + parent.name_err
+
+        # Fill members
+        self.string = loaded
+
+    def test_types(self):
+        self.tc.checkInstance(self, 'string', str)
+
+
+class Possibility:
+
+    def __init__(self, tc, i, loaded, parent):
+        # The test case
+        self.tc = tc
+
+        # Error message suffix
+        self.name_err = 'possibility #{} in '.format(i) + parent.name_err
+
+        # Fill members
+        self.string = loaded
+
+    def test_types(self):
+        self.tc.checkInstance(self, 'string', str)
+
+    # TODO: test values: there's only one |
+
+
 class MultipleChoiceDetails:
 
     def __init__(self, tc, loaded, parent):
@@ -156,46 +206,189 @@ class MultipleChoiceDetails:
 class MatrixChoiceDetails:
 
     def __init__(self, tc, loaded, parent):
-        pass
+        # The test case
+        self.tc = tc
+
+        # Error message suffix
+        self.name_err = 'matrixChoiceDetails in ' + parent.name_err
+
+        # Fill members
+        self.text = tc.checkIn(self, loaded, 'text')
+        self.choices = tc.checkIn(self, loaded, 'choices')
 
     def test_types(self):
-        pass
+        self.tc.checkInstance(self, 'text', str)
+        self.tc.checkInstance(self, 'choices', list)
+        for i, c in enumerate(self.choices):
+            Choice(self.tc, i, c, self).test_types()
+
+    # TODO: test values of 'choices'
 
 
 class AutoListDetails:
 
     def __init__(self, tc, loaded, parent):
-        pass
+        # The test case
+        self.tc = tc
+
+        # Error message suffix
+        self.name_err = 'autoListDetails in ' + parent.name_err
+
+        # Fill members
+        self.text = tc.checkIn(self, loaded, 'text')
+        self.hint = tc.checkIn(self, loaded, 'hint')
+        self.possibilities = tc.checkIn(self, loaded, 'possibilities')
 
     def test_types(self):
-        pass
+        self.tc.checkInstance(self, 'text', str)
+        self.tc.checkInstance(self, 'hint', str)
+        self.tc.checkInstance(self, 'possibilities', list)
+        for i, p in enumerate(self.possibilities):
+            Possibility(self.tc, i, p, self).test_types()
 
 
 class ManySlidersDetails:
 
     def __init__(self, tc, loaded, parent):
-        pass
+        # The test case
+        self.tc = tc
+
+        # Error message suffix
+        self.name_err = 'manySlidersDetails in ' + parent.name_err
+
+        # Fill members
+        self.text = tc.checkIn(self, loaded, 'text')
+        self.availableSliders = tc.checkIn(self, loaded, 'availableSliders')
+        self.defaultSliders = tc.checkIn(self, loaded, 'defaultSliders')
+        self.hints = tc.checkIn(self, loaded, 'hints')
+        self.addItemHint = tc.checkIn(self, loaded, 'addItemHint')
+        self.dialogText = tc.checkIn(self, loaded, 'dialogText')
+        self.showLiveIndication = loaded.get('showLiveIndication', None)
+        self.initialPosition = loaded.get('initialPosition', None)
 
     def test_types(self):
-        pass
+        self.tc.checkInstance(self, 'text', str)
+        self.tc.checkInstance(self, 'availableSliders', list)
+        for i, s in enumerate(self.availableSliders):
+            Slider(self.tc, i, s, self).test_types()
+        self.tc.checkInstance(self, 'defaultSliders', list)
+        for i, s in enumerate(self.defaultSliders):
+            Slider(self.tc, i, s, self).test_types()
+        self.tc.checkInstance(self, 'hints', list)
+        for i, h in enumerate(self.hints):
+            Hint(self.tc, i, h, self).test_types()
+        self.tc.checkInstance(self, 'addItemHint', str)
+        self.tc.checkInstance(self, 'dialogText', str)
+        if self.showLiveIndication is not None:
+            self.tc.checkInstance(self, 'showLiveIndication', bool)
+        if self.initialPosition is not None:
+            self.tc.checkInstance(self, 'initialPosition', int)
+
+    # TODO: test values. number of hints, defaultSliders in availableSliders.
+    # value of initialPosition
+
+
+class SliderSubQuestion:
+
+    def __init__(self, tc, i, loaded, parent):
+        # The test case
+        self.tc = tc
+
+        # Error message suffix
+        self.name_err = 'subQuestion #{} in '.format(i) + parent.name_err
+
+        # Fill members
+        self.text = tc.checkIn(self, loaded, 'text')
+        self.hints = tc.checkIn(self, loaded, 'hints')
+        self.notApplyAllowed = loaded.get('notApplyAllowed', None)
+        self.showLiveIndication = loaded.get('showLiveIndication', None)
+        self.initialPosition = loaded.get('initialPosition', None)
+
+    def test_types(self):
+        self.tc.checkInstance(self, 'text', str)
+        self.tc.checkInstance(self, 'hints', list)
+        for i, h in enumerate(self.hints):
+            Hint(self.tc, i, h, self).test_types()
+        if self.notApplyAllowed is not None:
+            self.tc.checkInstance(self, 'notApplyAllowed', bool)
+        if self.showLiveIndication is not None:
+            self.tc.checkInstance(self, 'showLiveIndication', bool)
+        if self.initialPosition is not None:
+            self.tc.checkInstance(self, 'initialPosition', int)
+
+    # TODO: test values: at least two hints, value of initialPosition
 
 
 class SliderDetails:
 
     def __init__(self, tc, loaded, parent):
-        pass
+        # The test case
+        self.tc = tc
+
+        # Error message suffix
+        self.name_err = 'sliderDetails in ' + parent.name_err
+
+        # Fill members
+        self.subQuestions = tc.checkIn(self, loaded, 'subQuestions')
 
     def test_types(self):
-        pass
+        for i, s in enumerate(self.subQuestions):
+            SliderSubQuestion(self.tc, i, s, self).test_types()
+
+
+class StarRatingSubQuestion:
+
+    def __init__(self, tc, i, loaded, parent):
+        # The test case
+        self.tc = tc
+
+        # Error message suffix
+        self.name_err = 'subQuestion #{} in '.format(i) + parent.name_err
+
+        # Fill members
+        self.text = tc.checkIn(self, loaded, 'text')
+        self.hints = tc.checkIn(self, loaded, 'hints')
+        self.notApplyAllowed = loaded.get('notApplyAllowed', None)
+        self.showLiveIndication = loaded.get('showLiveIndication', None)
+        self.numStars = loaded.get('numStars', None)
+        self.stepSize = loaded.get('stepSize', None)
+        self.initialRating = loaded.get('initialRating', None)
+
+    def test_types(self):
+        self.tc.checkInstance(self, 'text', str)
+        self.tc.checkInstance(self, 'hints', list)
+        for i, h in enumerate(self.hints):
+            Hint(self.tc, i, h, self).test_types()
+        if self.notApplyAllowed is not None:
+            self.tc.checkInstance(self, 'notApplyAllowed', bool)
+        if self.showLiveIndication is not None:
+            self.tc.checkInstance(self, 'showLiveIndication', bool)
+        if self.numStars is not None:
+            self.tc.checkInstance(self, 'numStars', int)
+        if self.stepSize is not None:
+            self.tc.checkInstance(self, 'stepSize', float)
+        if self.initialRating is not None:
+            self.tc.checkInstance(self, 'initialRating', float)
+
+    # TODO: test values: at least two hints,
+    # values of initialRating, numStars, stepSize
 
 
 class StarRatingDetails:
 
     def __init__(self, tc, loaded, parent):
-        pass
+        # The test case
+        self.tc = tc
+
+        # Error message suffix
+        self.name_err = 'starRatingDetails in ' + parent.name_err
+
+        # Fill members
+        self.subQuestions = tc.checkIn(self, loaded, 'subQuestions')
 
     def test_types(self):
-        pass
+        for i, s in enumerate(self.subQuestions):
+            StarRatingSubQuestion(self.tc, i, s, self).test_types()
 
 
 class Question:
@@ -227,6 +420,8 @@ class Question:
 
         details = self.type_classes[self.type](self.tc, self.details, self)
         details.test_types()
+
+    # TODO: test values of 'details'
 
 
 class bcolors:
